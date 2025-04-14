411118a2b1fa758a6c5c6aa9352122bfd554aaf0
Goob-tech <solizg02@gmail.com>
2025-04-14 14:40:47 -0500

    get party members and update party finished
    Created get party members command that will list out all party members of a party
    
    Update party record will let the user update the partys wins and losses count

--- commands/utility/get_party_members.js
+++ commands/utility/get_party_members.js
@@ -1,10 +1,80 @@
-import { SlashCommandBuilder } from 'discord.js'
+import { SlashCommandBuilder } from 'discord.js';
+import db_commands from '../../db_commands/db_commands.js';
+import 'dotenv/config';
+import pg from "pg";
 
 export default  {
 	data: new SlashCommandBuilder()
 		.setName('get_party_members')
-		.setDescription('Gets the members of a specified party'),
+		.setDescription('Gets the members of a specified party')
+		.addStringOption(option =>
+            option.setName('party_name')
+                .setDescription('The name of the party you wish to get the members of.')
+                .setRequired(true)
+        ),
 	async execute(interaction) {
-		await interaction.reply('Im gonna hand you a friggen packet yo!');
+		const partyName = interaction.options.getString("party_name");
+		const db = new pg.Client({
+			user: process.env.DB_USER,
+			host: process.env.DB_HOST,
+			database: process.env.DB_NAME,
+			password: process.env.DB_PASSWORD,
+			port: parseInt(process.env.DB_PORT),
+		});
+
+		let replyStr;
+		
+		db.connect();
+
+		try {
+			const partyData = await db_commands.getParty(partyName, db);
+			const userIds = await db.query("SELECT user_id FROM user_parties WHERE party_id = $1", [partyData.id]);
+
+			let idQuery = ""
+
+			userIds.rows.forEach(({user_id}, index) => {
+				if(index === userIds.rows.length - 1) {
+					idQuery += `${user_id}`;
+				} else {
+					idQuery += `${user_id}, `;
+				}
+			});
+
+			const partyMembersData = await db.query(`SELECT discord_id FROM users WHERE id in (${idQuery})`);
+			const memberNicknames = await getMemberNicknames(partyMembersData.rows, interaction);
+
+			if(memberNicknames !== undefined && memberNicknames.length > 0) {
+				replyStr = `The party members of ${partyName} are:`
+	
+				memberNicknames.forEach(nickname => {
+					replyStr += `\n ${nickname}`;
+				});
+			} else {
+				replyStr = "Failed to get party members";
+			}
+
+		} catch (error) {
+			console.error(error);
+			if(replyStr === undefined) {
+				replyStr = "Something went wrong";
+			}
+		}
+		
+		db.end();
+
+		await interaction.reply(replyStr);
 	},
 };
+
+async function getMemberNicknames(discordIds, interaction) {
+	const nicknames = [];
+			
+	for (let i = 0; i < discordIds.length; i++) {
+		const {discord_id} = discordIds[i];
+		const interactionUser = await interaction.guild.members.fetch(discord_id);
+		
+		nicknames.push(interactionUser.nickname);
+	}
+
+	return nicknames;
+}
--- commands/utility/update_party_record.js
+++ commands/utility/update_party_record.js
@@ -11,12 +11,17 @@ export default  {
             option.setName('party_name')
                 .setDescription('The name of the party you wish to update.')
                 .setRequired(true)
-        ),
+        )
+		.addBooleanOption(option => 
+			option.setName("did_you_win")
+				.setDescription("Select true for win and false for loss.")
+				.setRequired(true)
+		),
 	async execute(interaction) {
 		const interactionUser = await interaction.guild.members.fetch(interaction.user.id);
 		const partyName = interaction.options.getString('party_name');
+		const isWin = interaction.options.getBoolean('won_or_lost');
 		const userId = `@<${interactionUser.user.id}>`;
-
 		const db = new pg.Client({
 			user: process.env.DB_USER,
 			host: process.env.DB_HOST,
@@ -24,6 +29,8 @@ export default  {
 			password: process.env.DB_PASSWORD,
 			port: parseInt(process.env.DB_PORT),
 		});
+
+		let replyStr;
 		
 		db.connect();
 
@@ -31,20 +38,30 @@ export default  {
 			const party = await db_commands.getParty(partyName, db);
 
 			if(party.leader !== userId) {
+				replyStr = "Command failed. Only the party leader may update the party."
 				throw new Error("Person using command is not party leader, abort!");
 			}
 
-			//Update win loss
+			if(isWin) {
+				party.wins += 1;
+			} else {
+				party.losses += 1;
+			}
+
+			const updateParty = await db.query("UPDATE parties SET wins = $1, losses = $2 WHERE name = $3", [party.wins, party.losses, party.name]);
+
+			replyStr = "Successfully updated party record!";
 			
 		} catch (error) {
 			console.error(error.message);
-			await db.end();
-			await interaction.reply('Something went wrong :(!');
-			return;
+			
+			if(replyStr === undefined) {
+				replyStr = "Something went wrong :(!"
+			}
 		}
 
 		await db.end();
 
-		await interaction.reply('This command is in progress. Stop or i will hand you a friggen packet yo');
+		await interaction.reply(replyStr);
 	},
 };
